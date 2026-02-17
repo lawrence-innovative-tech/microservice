8415f63eff3ffb78ab459d1ec0564c7772b8856f
Lawrence <M719@eaglesoftware.com>
2026-02-17 12:18:01 +0530

    test integration spring 1

--- kafka-producer/src/test/java/com/microservice/kafka_producer/InjectMockTest.java
+++ kafka-producer/src/test/java/com/microservice/kafka_producer/InjectMockTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest(classes = KafkaProducerApplication.class)
+//@SpringBootTest(classes = KafkaProducerApplication.class)
 public class InjectMockTest {
 
     @Mock
